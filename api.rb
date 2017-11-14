a06299bc79e469c97c0d33dfa518ff1140eb59c3
getty104 <hayabusatoshihumi@gmail.com>
2017-11-14 18:25:36 +0900

    条件の修正

--- api.rb
+++ api.rb
@@ -14,25 +14,25 @@ def judge lang, code, input, ans
   case lang
   when 'c'
     file_name = "main.c"
-    container = create_container 'gcc:latest', file_name, code, input
-    ce = container.exec(["sh", "-c", "timeout -s 9 2 gcc #{file_name}"]).last == 0
+    container = create_container('gcc:latest', file_name, code, input)
+    ce = container.exec(["sh", "-c", "gcc #{file_name}"]).last != 0
     exec_cmd = './a.out'
   when 'cpp'
     file_name = "main.cpp"
-    container = create_container 'gcc:latest', file_name, code, input
-    ce = container.exec(["sh", "-c", "timeout -s 9 2 g++ #{file_name}"]).last == 0
+    container = create_container('gcc:latest', file_name, code, input)
+    ce = container.exec(["sh", "-c", "g++ #{file_name}"]).last != 0
     exec_cmd = './a.out'
   when 'py'
     file_name = "main.py"
-    container = create_container 'python:latest', file_name, code, input
+    container = create_container('python:latest', file_name, code, input)
     exec_cmd = "python #{file_name}"
   when 'rb'
     file_name = "main.rb"
-    container = create_container 'ruby:latest', file_name, code, input
+    container = create_container('ruby:latest', file_name, code, input)
     exec_cmd = "ruby #{file_name}"
   end
 
-  return 'CE' unless ce
+  return 'CE' if ce
   sleep(0.005)
   result = container.exec(["sh", "-c", "timeout -s 9 2 #{exec_cmd} < input.txt"])
   container.delete(force: true)
@@ -47,19 +47,20 @@ def judge lang, code, input, ans
 end
 
 def create_container image_name, file_name, code, input
-  memory = 128
+  memory = 500 * 1024 * 1024
+  data_size = 256 * 1024
   options = {
     'Image' => image_name,
     'Tty' => true,
     'HostConfig' => {
-      'Memory' => memory * 1024 * 1024,
+      'Memory' => memory ,
       'PidsLimit' => 10
     },
     'WorkingDir' => '/tmp'
   }
-
   container = Docker::Container.create(options)
   container.start
+  container.exec(["ulimit","-u","50","-d", "#{data_size}", "-t", "10"])
   container.store_file("/tmp/#{file_name}", code)
   container.store_file("/tmp/input.txt", input)
   return container
